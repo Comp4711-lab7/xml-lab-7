ee3b2d2e1b52e4b1c978468528391ab485137f6f
Jox <joxched@gmail.com>
2016-03-10 16:23:14 -0800

    add facets into columns

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,36 +1,45 @@
 
-<p>XML DATA GOES HERE</p>
-<table class="table">
+<div>
+    <h2>Search</h2>
 
-    <h2>Days of Week</h2>
-    {daysofweek}
-        COURSE:
-        {coursename}</br>
-        {day}: {time}</br>
-        {instructor}</br>
-        {building}: {room}</br>
-        {type}</br></br>
-   {/daysofweek}
+</div>
 
-    </br></br>
-    <h2>Periods</h2>
-    {periods}
-        COURSE:
-        {coursename}</br>
-        {day}: {time}</br>
-        {instructor}</br>
-        {building}: {room}</br>
-        {type}</br></br>
-    {/periods}
-    <hr>
 
-    <h2>Courses</h2>
-    {courses}
-        COURSE:
-        {coursename}</br>
-        {day}: {time}</br>
-        {instructor}</br>
-        {building}: {room}</br>
-        {type}</br></br>
-    {/courses}
-</table>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4">
+                <h2>Days of Week</h2>
+                {daysofweek}
+                COURSE:
+                {coursename}</br>
+                {day}: {time}</br>
+                {instructor}</br>
+                {building}: {room}</br>
+                {type}</br></br>
+                {/daysofweek}
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <h2>Periods</h2>
+                {periods}
+                COURSE:
+                {coursename}</br>
+                {day}: {time}</br>
+                {instructor}</br>
+                {building}: {room}</br>
+                {type}</br></br>
+                {/periods}
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <h2>Courses</h2>
+                {courses}
+                COURSE:
+                {coursename}</br>
+                {day}: {time}</br>
+                {instructor}</br>
+                {building}: {room}</br>
+                {type}</br></br>
+                {/courses}
+            </div>
+        </div>
+    </div>
