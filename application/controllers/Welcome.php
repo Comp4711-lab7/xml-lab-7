131cdb9dc29fb3543242f4da2e18496cf5d3b120
Jox <joxched@gmail.com>
2016-03-12 16:03:53 -0800

    fix result page output

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,68 +1,86 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends Application {
+class Welcome extends Application
+{
 
-	function __construct() {
-		parent::__construct();
-		$this->load->model('timetable');
-		$this->load->helper('form');
-	}
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('timetable');
+        $this->load->helper('form');
+    }
 
-	public function index()
-	{
+    public function index()
+    {
 
-		$this->data['title'] = 'XML Lab';
-		$this->data['pagebody'] = 'welcome';
-		$this->data['daysofweek'] = $this->timetable->getDaysOfWeek();
-		$this->data['periods'] = $this->timetable->getPeriods();
-		$this->data['courses'] = $this->timetable->getCourses();
-		$this->data['daysearch'] = form_dropdown('day',$this->timetable->getDays());
-		$this->data['timeslotsearch'] = form_dropdown('time',$this->timetable->getTimeSlots());
-		$this->render();
-	}
+        $this->data['title'] = 'XML Lab';
+        $this->data['pagebody'] = 'welcome';
+        $this->data['daysofweek'] = $this->timetable->getDaysOfWeek();
+        $this->data['periods'] = $this->timetable->getPeriods();
+        $this->data['courses'] = $this->timetable->getCourses();
+        $this->data['daysearch'] = form_dropdown('day', $this->timetable->getDays());
+        $this->data['timeslotsearch'] = form_dropdown('time', $this->timetable->getTimeSlots());
+        $this->render();
+    }
 
-	function results()
-	{
-            
-                
-                $c = $this->timetable->searchCourses($this->input->post('day'), $this->input->post('time'));
-                $p = $this->timetable->searchPeriods($this->input->post('day'), $this->input->post('time'));
-                $d = $this->timetable->searchDaysOfWeek($this->input->post('day'), $this->input->post('time'));
-                $this->data['cSearch'] = "no results";
-                $this->data['pSearch'] = "no results";
-                $this->data['dSearch'] = "no results";
-                    if($c){
-                        $this->data['cSearch'] = $this->BookingToString($c);
-                    }
-                    if($p){
-                        $this->data['pSearch'] = $this->BookingToString($p);
-                    }
-                    if($d){
-                        $this->data['dSearch'] = $this->BookingToString($d);
-                    }
-                
-                $this->data['bingo'] = "NOT A BINGO";
-                if($this->data['cSearch'] != "no results" && $this->data['pSearch'] != "no results" && $this->data['dSearch'] != "no results"){
-                    $this->data['bingo'] = "BINGO";
-                }
-                
-		$this->data['title'] = 'XML Lab Results';
-		$this->data['pagebody'] = 'results';
+    function results()
+    {
+        $c = $this->timetable->searchCourses($this->input->post('day'), $this->input->post('time'));
+        $p = $this->timetable->searchPeriods($this->input->post('day'), $this->input->post('time'));
+        $d = $this->timetable->searchDaysOfWeek($this->input->post('day'), $this->input->post('time'));
+//        $this->data['courses'] = "no results";
+//        $this->data['periods'] = "no results";
+//        $this->data['daysofweek'] = "no results";
 
-		// call search function
+        if ($c) {
+            $this->data['courses'] = $this->BookingToString($c);
+        }else{
+            $this->data['courses'] = array();
+        }
+        if ($p) {
+            $this->data['periods'] = $this->BookingToString($p);
+        }else{
+            $this->data['periods'] = array();
+        }
+        if ($d) {
+            $this->data['daysofweek'] = $this->BookingToString($d);
+        }else{
+            $this->data['daysofweek'] = array();
+        }
 
-		$this->render();
-	}
-        
-        
-            public function BookingToString($booking){
-                $string = "COURSE: " . $booking->coursename . PHP_EOL  
-                        . $booking->day  . " : " . $booking->time . PHP_EOL  
-                        . $booking->instructor . PHP_EOL  
-                        . $booking->building . " : " . $booking->room . PHP_EOL  
-                        . $booking->type;
-                return $string;
-                
+//        $this->data['bingo'] = "NOT A BINGO";
+//        if ($this->data['courses'] != "no results" && $this->data['periods'] != "no results" && $this->data['daysofweek'] != "no results") {
+//            $this->data['bingo'] = "BINGO";
+//        }
+
+        $this->data['title'] = 'XML Lab Results';
+        $this->data['pagebody'] = 'results';
+
+        // call search function
+
+        $this->render();
+    }
+
+
+    public function BookingToString($booking)
+    {
+//        $string = "COURSE: " . $booking->coursename . PHP_EOL
+//            . $booking->day . " : " . $booking->time . PHP_EOL
+//            . $booking->instructor . PHP_EOL
+//            . $booking->building . " : " . $booking->room . PHP_EOL
+//            . $booking->type;
+//        return $string;
+        $b = array();
+        $b['day'] = (string) $booking->day;
+        $b['time'] = (string) $booking->time;
+        $b['coursename'] = (string) $booking->coursename;
+        $b['instructor'] = (string) $booking->instructor;
+        $b['building'] = (string) $booking->building;
+        $b['room'] = (string) $booking->room;
+        $b['type'] = (string) $booking->type;
+
+        return array($b);
+
     }
 }
--- application/views/results.php
+++ application/views/results.php
@@ -1,19 +1,58 @@
+<!--<div class="container">-->
+<!--        {bingo}-->
+<!--        <div class="row">-->
+<!--            <div class="col-lg-4 col-md-4">-->
+<!--                <h2>Days of Week</h2>-->
+<!--                {dSearch}-->
+<!---->
+<!--            </div>-->
+<!--            <div class="col-lg-4 col-md-4">-->
+<!--                <h2>Periods</h2>-->
+<!--                {pSearch}-->
+<!---->
+<!--            </div>-->
+<!--            <div class="col-lg-4 col-md-4">-->
+<!--                <h2>Courses</h2>-->
+<!--                {cSearch}-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+
 <div class="container">
-        {bingo}
-        <div class="row">
-            <div class="col-lg-4 col-md-4">
-                <h2>Days of Week</h2>
-                {dSearch}
-
-            </div>
-            <div class="col-lg-4 col-md-4">
-                <h2>Periods</h2>
-                {pSearch}
-
-            </div>
-            <div class="col-lg-4 col-md-4">
-                <h2>Courses</h2>
-                {cSearch}
-            </div>
+<!--    {bingo}-->
+    <div class="row">
+        <div class="col-lg-4 col-md-4">
+            <h2>Days of Week</h2>
+            {daysofweek}
+            COURSE:
+            {coursename}</br>
+            {day}: {time}</br>
+            {instructor}</br>
+            {building}: {room}</br>
+            {type}</br></br>
+            {/daysofweek}
+        </div>
+        <div class="col-lg-4 col-md-4">
+            <h2>Periods</h2>
+            {periods}
+            COURSE:
+            {coursename}</br>
+            {day}: {time}</br>
+            {instructor}</br>
+            {building}: {room}</br>
+            {type}</br></br>
+            {/periods}
+        </div>
+        <div class="col-lg-4 col-md-4">
+            <h2>Courses</h2>
+            {courses}
+            COURSE:
+            {coursename}</br>
+            {day}: {time}</br>
+            {instructor}</br>
+            {building}: {room}</br>
+            {type}</br></br>
+            {/courses}
         </div>
     </div>
+</div>
